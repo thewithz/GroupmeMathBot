1ff94d5de73cd7add95f8ed377aa887a3a17da97
Nathan Withers <nathanwithz@gmail.com>
2017-09-29 17:23:04 -0400

    Added newton to the bot

--- app.py
+++ app.py
@@ -1,37 +1,61 @@
 import os
 import sys
 import json
+import groupy
+import pynewtonmath as newton
 
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
+from groupy import Group
+from groupy import Bot
+from groupy import Member
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    log('Recieved {}'.format(data))
-
-    # We don't want to reply to ourselves!
-    if data['name'] != 'testbot':
-        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-        send_message(msg)
-
-    return "ok", 200
-
-def send_message(msg):
-    url  = 'https://api.groupme.com/v3/bots/post'
-
-    data = {
-            'bot_id' : os.getenv('GROUPME_BOT_ID'),
-            'text'   : msg
-           }
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()
-                                    
-def log(msg):
-    print(str(msg))
-    sys.stdout.flush()
+acceptableMathList = ['simplify', 'factor', 'derive', 'integrate', 'zeroes', 'tangent', 'area', 'cos', 'sin', 'tan', 'arccos', 'arcsin', 'arctan', 'abs', 'log']
+group = Group.list().first
+bot = Bot.list().first
+bot.post('this is a test')
+# We don't want to reply to ourselves!
+if bot.name != 'testbot':
+    newMsg = group.messages().newest.text
+    msgArr = newMsg.split()
+    if msgArr[0] == '!help':
+        msg = 'help me!'
+        bot.post(msg)
+    elif msgArr[0] == '!math':
+        if !msgArr[1] in acceptableMathList:
+            msg = 'I couldn\'t understand which operation to perform'
+            bot.post(msg)
+        elif msgArr[0] == '!math':
+            if msgArr[1] == 'simplify':
+                bot.post(newton.simplify(msgArr[2]))
+            elif msgArr[1] == 'factor':
+                bot.post(newton.factor(msgArr[2]))
+            elif msgArr[1] == 'derive':
+                bot.post(newton.derive(msgArr[2]))
+            elif msgArr[1] == 'integrate':
+                bot.post(newton.integrate(msgArr[2]))
+            elif msgArr[1] == 'zeroes':
+                bot.post(newton.zeroes(msgArr[2]))
+            elif msgArr[1] == 'tangent':
+                bot.post(newton.tangent(msgArr[2], msgArr[3]))
+            elif msgArr[1] == 'area':
+                bot.post(newton.area(msgArr[2]), msgArr[3], msgArr[4])
+            elif msgArr[1] == 'cos':
+                bot.post(newton.cos(msgArr[2]))
+            elif msgArr[1] == 'sin':
+                bot.post(newton.sin(msgArr[2]))
+            elif msgArr[1] == 'tan':
+                bot.post(newton.tan(msgArr[2]))
+            elif msgArr[1] == 'arccos':
+                bot.post(newton.arccos(msgArr[2]))
+            elif msgArr[1] == 'arcsin':
+                bot.post(newton.arcsin(msgArr[2]))
+            elif msgArr[1] == 'arctan':
+                bot.post(newton.arctan(msgArr[2]))
+            elif msgArr[1] == 'abs':
+                bot.post(newton.abs(msgArr[2]))
+            elif msgArr[1] == 'log':
+                bot.post(newton.log(msgArr[2], msgArr[3]))
 
