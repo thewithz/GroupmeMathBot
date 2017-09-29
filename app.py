47689120cc0a0b0856c889dbd14e9bfc15e21e92
Nathan Withers <nathanwithz@gmail.com>
2017-09-29 15:09:54 -0400

    Last try... trailing comma is removed

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def send_message(msg):
 
     data = {
             'bot_id' : os.getenv('GROUPME_BOT_ID'),
-            'text'   : msg,
+            'text'   : msg
            }
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
