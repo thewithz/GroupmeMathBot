175e387e5f6c5a9451e8f65edafe1f485b05a1a4
Nathan Withers <nathanwithz@gmail.com>
2017-09-29 14:12:27 -0400

    Added some log code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
 import os
+import sys
 import json
 
 from urllib.parse import urlencode
@@ -12,20 +12,26 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
+    log('Recieved {}'.format(data))
 
     # We don't want to reply to ourselves!
     if data['name'] != 'apnorton-test-bot':
         msg = '{}, you sent "{}".'.format(data['name'], data['text'])
         send_message(msg)
-    
+
     return "ok", 200
 
 def send_message(msg):
     url  = 'https://api.groupme.com/v3/bots/post'
-    
+
     data = {
             'bot_id' : os.getenv('GROUPME_BOT_ID'),
             'text'   : msg,
            }
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
+                                    
+def log(msg):
+    print(str(msg))
+    sys.stdout.flush()
+
