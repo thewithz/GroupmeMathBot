414b0b970c8187f7a0d6ea6f0a85cd7e9602f1b0
Nathan Withers <nathanwithz@gmail.com>
2017-10-02 00:08:10 -0400

    still isnt working

--- app.py
+++ app.py
@@ -34,34 +34,34 @@ def webhook():
                 return 'ok', 200
             elif msgArr[0] == '!math':
                 if msgArr[1] == 'simplify':
-                    bot.post(newton.simplify(msgArr[2]))
+                    bot.post(newton.simplify(msgArr[2]).encode('utf-8'))
                 elif msgArr[1] == 'factor':
-                    bot.post(newton.factor(msgArr[2]))
+                    bot.post(newton.factor(msgArr[2]).encode('utf-8'))
                 elif msgArr[1] == 'derive':
-                    bot.post(newton.derive(msgArr[2]))
+                    bot.post(newton.derive(msgArr[2]).encode('utf-8'))
                 elif msgArr[1] == 'integrate':
-                    bot.post(newton.integrate(msgArr[2]))
+                    bot.post(newton.integrate(msgArr[2]).encode('utf-8'))
                 elif msgArr[1] == 'zeroes':
-                    bot.post(newton.zeroes(msgArr[2]))
+                    bot.post(newton.zeroes(msgArr[2]).encode('utf-8'))
                 elif msgArr[1] == 'tangent':
-                    bot.post(newton.tangent(msgArr[2], int(msgArr[3])))
+                    bot.post(newton.tangent(msgArr[2], int(msgArr[3])).encode('utf-8'))
                 elif msgArr[1] == 'area':
-                    bot.post(newton.area(msgArr[2], int(msgArr[3]), int(msgArr[4])))
+                    bot.post(newton.area(msgArr[2], int(msgArr[3]), int(msgArr[4])).encode('utf-8'))
                 elif msgArr[1] == 'cos':
-                    bot.post(newton.cos(int(msgArr[2])))
+                    bot.post(newton.cos(int(msgArr[2])).encode('utf-8'))
                 elif msgArr[1] == 'sin':
-                    bot.post(newton.sin(int(msgArr[2])))
+                    bot.post(newton.sin(int(msgArr[2])).encode('utf-8'))
                 elif msgArr[1] == 'tan':
-                    bot.post(newton.tan(int(msgArr[2])))
+                    bot.post(newton.tan(int(msgArr[2])).encode('utf-8'))
                 elif msgArr[1] == 'arccos':
-                    bot.post(newton.arccos(int(msgArr[2])))
+                    bot.post(newton.arccos(int(msgArr[2])).encode('utf-8'))
                 elif msgArr[1] == 'arcsin':
-                    bot.post(newton.arcsin(int(msgArr[2])))
+                    bot.post(newton.arcsin(int(msgArr[2])).encode('utf-8'))
                 elif msgArr[1] == 'arctan':
-                    bot.post(newton.arctan(int(msgArr[2])))
+                    bot.post(newton.arctan(int(msgArr[2])).encode('utf-8'))
                 elif msgArr[1] == 'abs':
-                    bot.post(newton.abs(int(msgArr[2])))
+                    bot.post(newton.abs(int(msgArr[2])).encode('utf-8'))
                 elif msgArr[1] == 'log':
-                    bot.post(newton.log(int(msgArr[2]), int(msgArr[3])))
+                    bot.post(newton.log(int(msgArr[2]), int(msgArr[3])).encode('utf-8'))
     return 'ok', 200
 
