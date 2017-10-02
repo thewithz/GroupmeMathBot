f0bccd24fc425b6a6c8273cc876ee65b7cb1754c
Nathan Withers <nathanwithz@gmail.com>
2017-10-02 19:47:00 -0400

    Fixed all the str errors

--- app.py
+++ app.py
@@ -34,34 +34,34 @@ def webhook():
                 return 'ok', 200
             elif msgArr[0] == '!math':
                 if msgArr[1] == 'simplify':
-                    bot.post(newton.simplify(msgArr[2]))
+                    bot.post(str(newton.simplify(msgArr[2])))
                 elif msgArr[1] == 'factor':
-                    bot.post(newton.factor(msgArr[2])[0])
+                    bot.post(str(newton.factor(msgArr[2])))
                 elif msgArr[1] == 'derive':
-                    bot.post(newton.derive(msgArr[2]))
+                    bot.post(str(newton.derive(msgArr[2])))
                 elif msgArr[1] == 'integrate':
-                    bot.post(newton.integrate(msgArr[2]))
+                    bot.post(str(newton.integrate(msgArr[2])))
                 elif msgArr[1] == 'zeroes':
-                    bot.post(newton.zeroes(msgArr[2]))
+                    bot.post(str(newton.zeroes(msgArr[2])))
                 elif msgArr[1] == 'tangent':
-                    bot.post(newton.tangent(msgArr[2], int(msgArr[3])))
+                    bot.post(str(newton.tangent(msgArr[2], int(msgArr[3]))))
                 elif msgArr[1] == 'area':
-                    bot.post(newton.area(msgArr[2], int(msgArr[3]), int(msgArr[4])))
+                    bot.post(str(newton.area(msgArr[2], int(msgArr[3]), int(msgArr[4]))))
                 elif msgArr[1] == 'cos':
-                    bot.post(newton.cos(msgArr[2]))
+                    bot.post(str(newton.cos(msgArr[2])))
                 elif msgArr[1] == 'sin':
-                    bot.post(newton.sin(msgArr[2]))
+                    bot.post(str(newton.sin(msgArr[2])))
                 elif msgArr[1] == 'tan':
-                    bot.post(newton.tan(msgArr[2]))
+                    bot.post(str(newton.tan(msgArr[2])))
                 elif msgArr[1] == 'arccos':
-                    bot.post(newton.arccos(msgArr[2]))
+                    bot.post(str(newton.arccos(msgArr[2])))
                 elif msgArr[1] == 'arcsin':
-                    bot.post(newton.arcsin(msgArr[2]))
+                    bot.post(str(newton.arcsin(msgArr[2])))
                 elif msgArr[1] == 'arctan':
-                    bot.post(newton.arctan(msgArr[2]))
+                    bot.post(str(newton.arctan(msgArr[2])))
                 elif msgArr[1] == 'abs':
-                    bot.post(newton.abs(msgArr[2]))
+                    bot.post(str(newton.abs(msgArr[2])))
                 elif msgArr[1] == 'log':
-                    bot.post(newton.log(int(msgArr[2]), int(msgArr[3])))
+                    bot.post(str(newton.log(int(msgArr[2]), int(msgArr[3]))))
     return 'ok', 200
 
